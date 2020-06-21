a67cf0980112ae16f427503fe39bf2e0dc81ffbb
nagyrobert92 <nagyrobert1992@yahoo.com>
2020-06-21 12:02:41 +0200

    routes created, tested with postman

--- server/index.js
+++ server/index.js
@@ -1,10 +1,15 @@
 const express = require("express");
 const app = express();
 
-app.get("/", function (req, res) {
+app.get("/", function(req, res) {
   res.send("Hello Kisso!");
 });
 
-app.listen(3000, function () {
+//Routes
+app.use("/users", require("./routes/users"));
+app.use("/auth", require("./routes/auth"));
+app.use("/profiles", require("./routes/profiles"));
+
+app.listen(3000, function() {
   console.log("app listening on port 3000!");
 });
